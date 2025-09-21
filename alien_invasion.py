2f40542bc2364792039ba1a2c6a54102c30d116c
Carlos Briceno <carlosbriceno33.2@gmail.com>
2025-09-20 23:10:48 -0700

    Added Ship Movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame # type: ignore
 from settings import Settings
-
+from ship import Ship
 
 
 class AlienInvasion:
@@ -23,21 +23,48 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self) # Make instance of Ship 
+
         # Set the background color 
         self.bg_color = (230, 230, 230)
     # Game is controlled by this function 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+            """Respond to keypresses and mouse events"""
             # Watch for keyboard and mouse events and act as a listener 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN: # Respond when key is actually pressed down 
+                    if event.key == pygame.K_RIGHT: # Check whether the right key is pressed
+                         self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                         self.ship.moving_left = True
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                          # Move ship to the right
+                        self.ship.rect.x += 1 # If right key is pressed, ship moves to the right 
             # Fill the screen with the background color with the fill() method 
             self.screen.fill(self.bg_color)
 
+
+    def _update_screen(self):
+            """Update images on the screen and flip to the new """
             # Access background color when filling up the screen 
             self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
 
             # Make the most recently drawn game visible 
             pygame.display.flip()
--- ship.py
+++ ship.py
@@ -4,17 +4,34 @@ class Ship:
     """Class to manage the ship"""
 
     def __init__(self, ai_game):
-        self.screen = ai_game.screen
+        self.screen = ai_game.screen # Assign the screen to ship 
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect 
         self.image = pygame.image.load('images/ship.bmp')
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect() # Call the it and give location of ship 
 
         # Start each ship at the very bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exaact horizontal position
+        self.x = float(self.rect.x)
+        # Movement flag start with the ship not moving 
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_right: # Move ship right if the flag is true 
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left:
+            self.rect.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
+
+    # Define blitme method to draw the image to the screen at the position 
+    # specified by self.rect
     def blitme(self):
         """Draw ship at current location"""
         self.screen.blit(self.image, self.rect)
-        
